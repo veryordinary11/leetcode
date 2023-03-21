19be55fafa2548f6abbb531671d98253b641fd79
veryordinary11 <bessingeralexisi@gmail.com>
2023-03-21 13:53:25 +0800

    Update 99_Recover Binary Search Tree.cpp

--- Binary Tree/99_Recover Binary Search Tree.cpp
+++ Binary Tree/99_Recover Binary Search Tree.cpp
@@ -88,3 +88,41 @@ public:
         change_tree(root,sample_tree);
         }
 };
+
+————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+    TreeNode* prev = new TreeNode(INT_MIN);
+
+    void Inorder(TreeNode* root){
+        if(root == NULL)    return;
+
+        Inorder(root->left);
+
+        if(first == NULL && prev->val > root->val) first = prev;
+        if(first != NULL && prev->val > root->val)    second = root;
+        prev = root;
+
+        Inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        Inorder(root);
+        swap(first->val, second->val);
+    }
+};
